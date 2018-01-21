d6dddc0f2859cd6e3aca1502081820506287e36d
Don Kim <dgkimdev@gmail.com>
2018-01-21 23:26:56 +0900

    Add battery
    Add `status_battery` to check capacity% of the battery.
    From now on I have multiple functions which push a string to the
    string `status` via `snprintf`, I need to check the length of each
    output string (output of `snprintf`) and update the location of `status`
    to be updated.
    Find a unicode (extended) character to display the battery icon.

--- dwmstatus.c
+++ dwmstatus.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <X11/Xlib.h>
 
 #define MAXBUF 1024
 
 const char *status_datetime(const char *);
+int status_battery();
 
 int main(int argc, char *argv[]) {
   Display *dpy = XOpenDisplay(NULL);
   const struct timespec sleeptime = {1, 0L};
+  size_t len;
   char status[MAXBUF];
 
   if (!dpy) {
@@ -17,7 +20,13 @@ int main(int argc, char *argv[]) {
   }
 
   while (1) {
-    snprintf(status, sizeof(status), "\u23F0 %s ", status_datetime("%T %z %a %F"));
+    len = 0;
+    len += snprintf(status, sizeof(status),
+                    "\u23F0%s ",
+                    status_datetime("%T%z %a %F"));
+    len += snprintf(status+len, sizeof(status)-len,
+                    "\U0001F50B%d%% ",
+                    status_battery());
 
     XStoreName(dpy, DefaultRootWindow(dpy), status);
     XSync(dpy, 0);
@@ -37,4 +46,15 @@ const char *status_datetime(const char *fmt) {
   if (strftime(localbuf, sizeof(localbuf), fmt, localtime(&t)) == 0)
     return NULL;
   return localbuf;
+}
+
+int status_battery() {
+  FILE *fp = fopen("/sys/class/power_supply/BAT0/capacity", "r");
+  if (!fp)
+    exit(1);
+
+  int capacity;
+  fscanf(fp, "%i", &capacity);
+  fclose(fp);
+  return capacity;
 }
