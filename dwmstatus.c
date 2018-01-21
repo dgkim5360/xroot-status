b361f39b44b3f3534755664720584ecb11a2b539
Don Kim <dgkimdev@gmail.com>
2018-01-21 22:23:32 +0900

    Add datetime
    Implement `status_datetime` function, and it has been easy so far.
    Add a unicode character for the clock icon.

--- dwmstatus.c
+++ dwmstatus.c
@@ -2,17 +2,24 @@
 #include <time.h>
 #include <X11/Xlib.h>
 
+#define MAXBUF 1024
+
+const char *status_datetime(const char *);
+
 int main(int argc, char *argv[]) {
   Display *dpy = XOpenDisplay(NULL);
   const struct timespec sleeptime = {1, 0L};
+  char status[MAXBUF];
 
   if (!dpy) {
     fprintf(stderr, "ERROR: cannot open display\n");
-     return 1;
+      return 1;
   }
 
   while (1) {
-    XStoreName(dpy, DefaultRootWindow(dpy), "hello!");
+    snprintf(status, sizeof(status), "\u23F0 %s ", status_datetime("%T %z %a %F"));
+
+    XStoreName(dpy, DefaultRootWindow(dpy), status);
     XSync(dpy, 0);
 
     nanosleep(&sleeptime, NULL);
@@ -20,4 +27,14 @@ int main(int argc, char *argv[]) {
 
   XCloseDisplay(dpy);
   return 0;
+}
+
+static char localbuf[MAXBUF];
+
+const char *status_datetime(const char *fmt) {
+  time_t t = time(NULL);
+
+  if (strftime(localbuf, sizeof(localbuf), fmt, localtime(&t)) == 0)
+    return NULL;
+  return localbuf;
 }
